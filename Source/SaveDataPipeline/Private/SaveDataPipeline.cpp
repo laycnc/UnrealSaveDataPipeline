e4f1894474d17274a071a9810486a91473cd6871
laycnc <getsingleton@gmail.com>
2023-01-16 21:32:11 +0900

    モジュールコードの整頓

--- Source/SaveDataPipeline/Private/SaveDataPipeline.cpp
+++ Source/SaveDataPipeline/Private/SaveDataPipeline.cpp
@@ -1,8 +1,18 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#include "SaveDataPipeline.h"
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
 
 #define LOCTEXT_NAMESPACE "FSaveDataPipelineModule"
+
+class FSaveDataPipelineModule : public IModuleInterface
+{
+public:
+
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
 
 void FSaveDataPipelineModule::StartupModule()
 {
